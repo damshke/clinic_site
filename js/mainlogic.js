e3f3a9b0e49680af4b48780757c4262674503f86
damshke <degorych25@mail.ru>
2023-09-20 12:07:45 +0300

    fix menu position and align the segments

--- js/mainlogic.js
+++ js/mainlogic.js
@@ -11,21 +11,21 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-}
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     if (n > slides.length) { slideIndex = 1 }
+//     if (n < 1) { slideIndex = slides.length }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += " active";
+//     captionText.innerHTML = dots[slideIndex - 1].alt;
+// }
 
 function goToCertificates() {
 
@@ -35,3 +35,12 @@ function goToCertificates() {
         certificatesSection.scrollIntoView({ behavior: "smooth" });
     }
 }
+
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('site-header');
+    if (window.scrollY > 50) {
+        header.classList.add('scroll');
+    } else {
+        header.classList.remove('scroll');
+    }
+});
