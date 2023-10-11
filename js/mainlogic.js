262fcaafbdff498db01ffb434ef14ee4c0fdb6ee
damshke <degorych25@mail.ru>
2023-10-11 12:30:27 +0300

    fix slider

--- js/mainlogic.js
+++ js/mainlogic.js
@@ -1,5 +1,7 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+window.addEventListener('load', () => {
+    showSlides(slideIndex);
+  });
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -11,18 +13,13 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
+    var slides = document.getElementsByClassName("slide");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
 
 function goToCertificates() {
